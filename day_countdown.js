0263c014b9d11781d3d4d263ed045fc8f5975046
Max W Lobato <ml93117@srcs.loc>
2018-02-15 12:15:38 -0800

    day coundown (broken)

---
+++ day_countdown.js
@@ -0,0 +1,40 @@
+var countdownButton = document.getElementById('getDayCountdown');
+
+function birthdayCountDown(bdate) {
+
+    var today = new Date();
+    var bday = new Date(bdate);
+    var currenttime = today.getTime();
+    var btime = bday.getTime();
+
+    if ((btime - currenttime) < 0) {
+
+
+        bday.setFullYear(today.getFullYear());
+
+        if ((bday.getTime() - today.getTime()) < 0) {
+            bday.setFullYear(bday.getFullYear() + 1);
+        }
+
+    }
+
+    var daysuntil = Math.ceil((bday.getTime() / 86400000) - (today.getTime() / 86400000));
+
+return daysuntil;
+
+
+}
+
+
+
+
+function displayDays(){
+    var input = document.getElementById('inputDate').value;
+    var result = birthdayCountDown(input);
+
+    document.getElementById('signText').textContent = result;
+}
+
+countdownButton.addEventListener('click', getDayCountdown);
+
+
