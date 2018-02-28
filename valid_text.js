90f381aee1a33034f8bc85e1b00abaebde9ea888
Max W Lobato <ml93117@srcs.loc>
2018-02-28 11:17:11 -0800

    attempt

--- valid_text.js
+++ valid_text.js
@@ -3,3 +3,35 @@ var textButton = document.getElementById('age');
 function validText(){
     if ()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+var countdownButton = document.getElementById('getDayCountdown');
+var signButton = document.getElementById('getSign');
+var agebutton = document.getElementById('calculateAge');
+
+
+
+function displayerror() {
+    var date = document.getElementById('inputDate').value;
+    var sorry = "Sorry that is an invalid date. Please try again.";
+    if (new Date(date).getDay == NaN) {
+        document.getElementById('birthdayCounddowndays').textContent = sorry;
+        document.getElementById('signText').textContent = sorry;
+        document.getElementById('age').textContent = sorry;
+    }
+}
+
+
+countdownButton.addEventListener('click', displayerror);
+signButton.addEventListener('click', displayerror);
+agebutton.addEventListener('click', displayerror);
