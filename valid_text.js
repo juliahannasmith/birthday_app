5545edc1816233cf206366cc5f5f5d677fc774c7
Max W Lobato <ml93117@srcs.loc>
2018-02-28 12:14:22 -0800

    edit testifdate

--- valid_text.js
+++ valid_text.js
@@ -24,10 +24,11 @@ var agebutton1 = document.getElementById('calculateAge');
 function displayerror() {
     var date = document.getElementById('inputDate').value;
     var sorry = "Sorry that is an invalid date. Please try again.";
-    if (isNaN(new Date(date).getTime) == "true") {
+    if (isNaN(new Date(date).getTime) == true || date.length > 10 || date.length < 8) {
         document.getElementById('errorText').textContent = sorry;
 
     }
+
 }
 
 
