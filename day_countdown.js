744007d6bac0af9f24953b6dcd25154ea0f351b2
Max W Lobato <ml93117@srcs.loc>
2018-02-28 11:17:58 -0800

    day cleanup

--- day_countdown.js
+++ day_countdown.js
@@ -1,6 +1,5 @@
 var countdownButton = document.getElementById('getDayCountdown');
-var signButton = document.getElementById('getSign');
-var agebutton = document.getElementById('calculateAge');
+
 
 function birthdayCountDown() {
 
@@ -39,17 +38,3 @@ function displayDays() {
 countdownButton.addEventListener('click', displayDays);
 
 
-function displayerror() {
-    var date = document.getElementById('inputDate').value;
-    var sorry = "Sorry that is an invalid date. Please try again.";
-    if (new Date(date).getDay == NaN) {
-        document.getElementById('birthdayCounddowndays').textContent = sorry;
-        document.getElementById('signText').textContent = sorry;
-        document.getElementById('age').textContent = sorry;
-    }
-}
-
-
-countdownButton.addEventListener('click', displayerror);
-signButton.addEventListener('click', displayerror);
-agebutton.addEventListener('click', displayerror);
