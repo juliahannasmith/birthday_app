b453e3d9f745d552950f94f9c29615ecc8dde6bc
Claudia  Munoz <cm16321@srcs.loc>
2018-03-02 10:13:16 -0800

    Added Age function on birthday.js file

--- birthday.js
+++ birthday.js
@@ -98,3 +98,20 @@ function displayDays(){
 
 signButton.addEventListener('click', getSign);
 
+
+
+// AGE!!!!
+var agebutton = document.getElementById("calculateAge");
+function getAge() {
+    var today = new Date();
+    var birthdate = new Date(document.getElementById('inputDate').value);
+    var age = Math.floor((today - birthdate)/ (360 * 24 * 60 * 60 * 1000));
+
+    return "You are " + age + " years old!";
+}
+function displayAge(){
+    var result = getAge();
+
+    document.getElementById('age').textContent = result;
+}
+agebutton.addEventListener('click', displayAge);
