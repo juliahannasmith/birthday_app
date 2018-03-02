e2d6a3bf279157a7c8ce3e0ada447ff26e4d8c0d
Max W Lobato <ml93117@srcs.loc>
2018-03-02 10:11:07 -0800

    added day countdown to master js

--- birthday.js
+++ birthday.js
@@ -59,6 +59,36 @@ function getSign() {
 
 
 
+function birthdayCountDown() {
+
+    var today = new Date();
+    var bday = new Date(document.getElementById('inputDate').value);
+    var currenttime = today.getTime();
+    var btime = bday.getTime();
+
+    if ((btime - currenttime) < 0) {
+
+
+        bday.setFullYear(today.getFullYear());
+
+        if ((bday.getTime() - today.getTime()) < 0) {
+            bday.setFullYear(bday.getFullYear() + 1);
+        }
+
+    }
+
+    var daysuntil = Math.ceil((bday.getTime() / 86400000) - (today.getTime() / 86400000));
+
+    return daysuntil;
+
+
+}
+
+
+
+
+
+
 function displayDays(){
     var input = document.getElementById('inputDate').value;
     var result = getSign(input);
@@ -68,10 +98,3 @@ function displayDays(){
 
 signButton.addEventListener('click', getSign);
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
->>>>>>> origin/master
